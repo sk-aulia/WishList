cc028097cea5b47b86b1fc997d12fc93ac6a73d4
Siti Kholisah Aulia <sitikholisahaulia00@gmail.com>
2023-04-11 22:29:09 +0700

    dotenv setup

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 client = MongoClient('mongodb://test:sparta@ac-zw21lce-shard-00-00.8e6hmnk.mongodb.net:27017,ac-zw21lce-shard-00-01.8e6hmnk.mongodb.net:27017,ac-zw21lce-shard-00-02.8e6hmnk.mongodb.net:27017/?ssl=true&replicaSet=atlas-19435k-shard-0&authSource=admin&retryWrites=true&w=majority')
 db = client.dbsparta
